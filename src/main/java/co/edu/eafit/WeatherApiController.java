387afdac339492bf90e614a029f36cbef8a9a941
Juan Felipe Gomez V <juan.gomez@sofka.com.co>
2022-02-08 16:51:30 -0500

    se eliminan import que no se utilizan

--- src/main/java/co/edu/eafit/WeatherApiController.java
+++ src/main/java/co/edu/eafit/WeatherApiController.java
@@ -1,7 +1,5 @@
 package co.edu.eafit;
 
-import co.edu.eafit.mongodb.StatisticRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
